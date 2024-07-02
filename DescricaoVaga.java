0d76335cbd75fa7cf000d2eea60ae4008bdf10b2
Mariana Goncalves <mari.goncalves004@gmail.com>
2024-07-01 22:49:02 -0300

    classe DescricaoVaga criada

---
+++ DescricaoVaga.java
@@ -0,0 +1,10 @@
+public class DescricaoVaga {
+    private int id;
+    private String titulo;
+    private String descricao;
+    private String requisito;
+    private String salario;
+    private String cargaHoraria;
+    private String tipoContratacao;
+    private String modalidade;
+}
