7f6b3b70bda015ce4995c17a63ca94813ab2cd7d
x578684@gruposantander.com <x578684@gruposantander.com>
2024-07-03 00:51:24 -0300

    add getters and setters

--- Pessoa.java
+++ Pessoa.java
@@ -1,8 +1,71 @@
 public class Pessoa {
     private String nome;
-    private String cpf;
+    private int cpf;
+    private String endereco;  
     private String email;
     private String telefone;
 
-    
+/**
+ * @return the nome
+ */
+public String getNome() {
+    return nome;
+}
+
+/**
+ * @param nome the nome to set
+ */
+public void setNome(String nome) {
+    this.nome = nome;
+}
+
+/**
+ * @param cpf the cpf to set
+ */
+public void setCpf(int cpf) {
+    this.cpf = cpf;
+}
+
+/**
+ * @return the endereco
+ */
+public String getEndereco() {
+    return endereco;
+}
+
+/**
+ * @param endereco the endereco to set
+ */
+public void setEndereco(String endereco) {
+    this.endereco = endereco;
+}
+
+/**
+ * @return the email
+ */
+public String getEmail() {
+    return email;
+}
+
+/**
+ * @param email the email to set
+ */
+public void setEmail(String email) {
+    this.email = email;
+}
+
+/**
+ * @return the telefone
+ */
+public String getTelefone() {
+    return telefone;
+}
+
+/**
+ * @param telefone the telefone to set
+ */
+public void setTelefone(String telefone) {
+    this.telefone = telefone;
+}
+
 }
