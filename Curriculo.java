bdaa244524f94a3f40adc18c06562bbd2d7d4f75
Phelion <pedrohenrique200683@gmail.com>
2024-07-07 10:38:34 -0300

    Create Curriculo.java

---
+++ Curriculo.java
@@ -0,0 +1,12 @@
+public class Curriculo {
+  String formacao;
+  String nivelEscolaridade;
+  String objetivos;
+  String habilidades;
+  String experiencia;
+  String cursos;
+  }
+
+//+cadastrar
+//+excluirEnquantoStatusCandidatouse
+//+editarEnquantoStatusCandidatouse
