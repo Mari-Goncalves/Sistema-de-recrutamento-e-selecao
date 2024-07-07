35e57f4107aa9f38421e82d60ceabd4e929baa01
dbziv <147110388+dbziv@users.noreply.github.com>
2024-07-07 01:44:08 -0300

    Adicionado menu, vaga e Main Scanner. Main agora mostra o menu.

--- Main.java
+++ Main.java
@@ -1,6 +1,17 @@
+import java.util.Scanner;
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Ola mundo!");
-        System.out.println("Ola mundo 2 !");
-    }
+
+  public static final Scanner leitor = new Scanner(System.in); //criado main scanner na main branch, lê todas as classes desde que o mesmo seja chamado usando o getLeito(); metodo.
+  public void main(String[] args) {
+        
+    Vaga rh = new Vaga();
+    Menu menu = new Menu(rh);
+      
+    Mainmenu.displayMenu();
+  }
+  public static Scanner getLeitor(){
+    return leitor;
+    
+  }
+  
 }
