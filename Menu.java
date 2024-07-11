af63b048bf7bd9ff3540c89d08d8abb600a45fef
dbziv <147110388+dbziv@users.noreply.github.com>
2024-07-11 00:52:40 -0300

    Contem menu principal e submenus com seus metodos proprios

--- Menu.java
+++ Menu.java
@@ -2,108 +2,144 @@ import java.lang.Math;
 import java.util.Scanner;
 
 public class Menu {
-  private Vaga rh;
+  private Vaga vg;
+  private Notificacao ntf;
+  private Empresa ugh;
   Scanner leitor = Main.getLeitor();
-  public Menu(Vaga rh){
-    this.rh = rh;
-    
-  }
+  boolean sair = false;
+  
+  //public Menu(Vaga rh){
+    //this.rh = rh;
+
+  //}
   
       
       public void displayMainMenu(){
-        boolean sair = false;
+        
         while(!sair){
-        System.out.println("Bem vindo ao Vaga Feliz!/n1 - Cadastro candidato/n2 - Cadastro Recrutador        /n3 - Login\n4 - Sair");
+        System.out.println("Bem vindo ao Vaga Feliz!/n1 - Cadastro candidato/n2 - Cadastro Recrutador/n3 - Login\n4 - Sair");
          int choice = leitor.nextInt();
           switch(choice){
             case 1:
-              
-              //if candidato, vai para cadastro e upload de curriculo, pós volta para menu abaixo
-              
-              System.out.println("1 - Ir para entrevistas\n2 - Acompanhe Candidaturas\n3 - Procurar Vagas\n 4 - Sair");
-              int input = leitor.nextInt();
-              if(input == 1){
-                
-                //vai para entrevista, se não tem nenhuma, tela em branco, opção de voltar.
-                
-              }else if(input == 2){
-                
-                //vai para as candidaturas, se nao tem nenhuma, tela em branco, opção de voltar.
-                
-              }if(input == 3){
-                rh.displayVagas();          
-              }else{
-                sair = true;
-              }
+              //candidato, vai para cadastro e upload de curriculo, pós volta para menu abaixo
+              MenuCandidato();
             case 2:
-              
               //if recrutador, vai para cadastro de empresa, e após vai para o menu abaixo
-              
-              System.out.println("1 - Gerenciamento de Vagas\n2 - Candidaturas\n3 - Entrevistamento\n 4 - Sair");
-
-              input = leitor.nextInt();
-              if(input == 1){
-                    displayCDV();
-              }else if(input == 2){
-                
-                //vai para candidaturas, se nao tem nenhuma, tela em branco, opção de voltar.
-                
-              }if(input == 3){
-                
-                //vai para entrevistamento, se não tem nenhuma, tela em branco, opção de voltar.
-                
-              }else{
-                sair = true;
-              }
-              
-            case 3:  
-              
-              //Login leva para um dos menus se login estiver correto. 
-              
+              MenuRecrutador();
+            case 3: 
+              //metodo do login
+              //Login leva para um dos menus. 
             case 4:
               System.out.println("*close the app*");
               sair = true;        
           }
         }
-        
-        
 
-      public DescricaoVaga displayCDV(){
-        //CDV = criador de vaga
 
-      Scanner leitor = Main.getLeitor();
-      int id = (int)(Math.random() * 1000);
-      System.out.println("(" + id + ")");
-      String titulo = leitor.nextLine();
-      System.out.println("Titulo: ");
-      String salario = leitor.nextLine();
-      System.out.println("\nSalario: ");
-      String descricao = leitor.nextLine();
-      System.out.println("\nDescricao: ");
-      String requisito = leitor.nextLine();
-      System.out.println("\nRequisitos: ");
-      String cargaHoraria = leitor.nextLine();
-      System.out.println("\nCarga Horaria: ");
-      String tipoContratacao = leitor.nextLine();
-      System.out.println("\nTipo de Contratacao: ");
-      String modalidade = leitor.nextLine();
-      System.out.println("\nModalidade: ");
-      DescricaoVaga vaga = new DescricaoVaga(id, titulo, descricao, requisito, salario, cargaHoraria, tipoContratacao, modalidade);
-      int choice = leitor.nextInt();
-      System.out.println("Confirmar? \n1 - Sim\n2 - Nao");
-          if(choice == 1){
-          //salva vaga em arquivo txt
-          rh.addVaga(vaga);
-            return vaga;
-      }else{
-      System.out.println("Vaga excluída.");
-        return vaga;
-        
+
+
+      private void CadastramentoCandidato(){
+
+        //metodo de login
+        // id e senha percorrem ambas listas, se id e senha match com algum dos logins do sistema (array list), ele entra
+        //se o login e senha estiverem na lista de candidatos, menu de candidato. E vice versa
         
       }
-    
-      
-    
+
+      private void CadastramentoRecrutador(){
+        
+        //metodo login acima
+        
+      }
+
+      public void MenuCandidato(){
+
+        System.out.println("1 - Ir para entrevistas\n2 - Acompanhe Candidaturas\n3 - Procurar Vagas\n4 - Notificações\n5 - Sair");
+        int input = leitor.nextInt();
+        switch(input){
+          case 1:
+            //vai para entrevista, se não tem nenhuma, tela em branco, opção de voltar.
+          case 2:
+          //vai para as candidaturas, se nao tem nenhuma, tela em branco, opção de voltar.
+          case 3:
+          vg.displayVagas();          
+          case 4: 
+          ntf.displayNotificacoes();
+          case 5:
+            System.out.println("Fechando aplicativo...");
+            sair = true;
+            
+        }
+        
+      }
+
+      private void MenuRecrutador(){
+
+      System.out.println("1 - Gerenciamento de Vagas\n2 - Candidaturas\n3 - Entrevistamento\n4 - Notificações \n5 - Sair");
+        int input = leitor.nextInt();
+        switch(input){
+          case 1:
+            displayGerenciador(); //criar um menu/metodo para gerenciar a entrada e saida de vagas
+            //gerenciamento de vagas vai para adicionar ou excluir vagas.
+            break;
+          case 2:
+            System.out.println("a");
+          //vai para as candidaturas, se nao tem nenhuma, tela em branco, opção de voltar.
+            break;
+          case 3:
+          vg.displayVagas();
+            break;
+          case 4: 
+          ntf.displayNotificacoes();
+            break;
+          case 5:
+            System.out.println("Fechando aplicativo...");
+            sair = true;
+            break;
+          default:
+            System.out.println("Opção inválida. Tente novamente.\n");
+            MenuRecrutador();
+
+        }
+        
+      }
+      public void displayGerenciador(){
+        
+        System.out.println("_GERENCIADOR DE VAGAS_\n1 - Adicionar Vaga\n2 - Excluir Vaga\n3 - Editar Vaga\n4 - Voltar");
+        int choice = leitor.nextInt();
+        switch(choice){
+          case 1:
+            vg.getDisplayCDV();
+            break;
+          case 2:
+            vg.getDeletarVaga(); 
+            break;
+          case 3:
+            vg.getEditarVaga();
+            break;
+          case 4:
+            MenuRecrutador();
+            
+          default:
+            System.out.println("Opção inválida");
+             MenuRecrutador();
+            
+        }
+                
     }
+
+        
+        public void getDisplayGerenciador(){
+          displayGerenciador();       
+        }
+        public void getMenuRecrutador(){
+          MenuRecrutador();       
+        }
+        
+
+     
   }
+  
+  
+
   
