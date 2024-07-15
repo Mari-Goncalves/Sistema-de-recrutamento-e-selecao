f404937b3a5f4b35c4b9f1d2bbe7e528aa3e8cb6
Phelion <pedrohenrique200683@gmail.com>
2024-07-15 12:23:41 -0300

    Update Curriculo.java

--- Curriculo.java
+++ Curriculo.java
@@ -1,12 +1,183 @@
-public class Curriculo {
-    private String nivelEscolaridade; 
-    private String objetivos; 
-    private String habilidades; 
-    private String experiencia; 
-    private String cursos; 
-    private boolean statusCandidatouse; 
+import java.util.Scanner;
 
-    // Construtor da classe Curriculo
+public class Curriculo {
+    private String formacao;
+    private String nivelEscolaridade;
+    private String objetivos;
+    private String habilidades;
+    private String experiencias;
+    private String nome;
+
+  Scanner sc = new Scanner(System.in);
+
+  public void CriarCurriculo() {
+    System.out.println("--------------------------------------------------");
+
+    System.out.print("Nome: ");
+    String nome = sc.nextLine();
+
+    System.out.println(" ");
+
+    System.out.print("Formação: ");
+    String formacaoCandidato = sc.nextLine();
+
+    System.out.println(" ");
+
+    System.out.print("Nível de escolaridade: ");
+    String nivelEscolaridadeCandidato = sc.nextLine();
+
+    System.out.println(" ");
+
+    System.out.print("Objetivos: ");
+    String objetivosCandidato = sc.nextLine();
+
+    System.out.println(" ");
+
+    System.out.print("Habilidades: ");
+    String habilidadesCandidato = sc.nextLine();
+
+    System.out.println(" ");
+
+    System.out.print("Experiências: ");
+    String experienciasCandidato = sc.nextLine();
+
+    System.out.println(" ");
+
+    System.out.print("Curriculo criado com sucesso! \n");
+    System.out.println("--------------------------------------------------");
+
+    this.formacao = formacaoCandidato;
+    this.nivelEscolaridade = nivelEscolaridadeCandidato;
+    this.objetivos = objetivosCandidato;
+    this.habilidades = habilidadesCandidato;
+    this.experiencias = experienciasCandidato;
+    this.nome = nome;
+  }
+
+  public void VerResultadoCurriculo() {
+    System.out.println("--------------------------------------------------");
+
+    System.out.printf("Nome: %s \n", nome);
+    System.out.println(" ");
+
+    System.out.printf("Formação: %s \n", formacao);
+    System.out.println(" ");
+
+    System.out.printf("Nível de escolaridade: %s \n", nivelEscolaridade);
+    System.out.println(" ");
+
+    System.out.printf("Objetivos: %s \n", objetivos);
+    System.out.println(" ");
+
+    System.out.printf("Habilidades: %s \n", habilidades);
+    System.out.println(" ");
+
+    System.out.printf("Experiências: %s \n", experiencias);
+    System.out.println(" ");
+
+    System.out.println("--------------------------------------------------");
+  }
+
+  public void AtualizarCurriculo() {
+    System.out.println("--------------------------------------------------");
+
+    System.out.print("Nome: ");
+    String nome = sc.nextLine();
+    setNome(nome);
+
+    System.out.println(" ");
+
+    System.out.print("Formação: ");
+    String formacaoCandidato = sc.nextLine();
+    setFormacao(formacaoCandidato);
+
+    System.out.println(" ");
+
+    System.out.print("Nível de escolaridade: ");
+    String nivelEscolaridadeCandidato = sc.nextLine();
+    setNivelEscolaridade(nivelEscolaridadeCandidato);
+
+    System.out.println(" ");
+
+    System.out.print("Objetivos: ");
+    String objetivosCandidato = sc.nextLine();
+    setObjetivos(objetivosCandidato);
+
+    System.out.println(" ");
+
+    System.out.print("Habilidades: ");
+    String habilidadesCandidato = sc.nextLine();
+    setHabilidades(habilidadesCandidato);
+
+    System.out.println(" ");
+
+    System.out.print("Experiências: ");
+    String experienciasCandidato = sc.nextLine();
+    setExperiencias(experienciasCandidato);
+
+    System.out.println(" ");
+
+    System.out.print("Curriculo atualizado com sucesso! \n");
+    System.out.println("--------------------------------------------------");
+
+    this.formacao = formacaoCandidato;
+    this.nivelEscolaridade = nivelEscolaridadeCandidato;
+    this.objetivos = objetivosCandidato;
+    this.habilidades = habilidadesCandidato;
+    this.experiencias = experienciasCandidato;
+    this.nome = nome;
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public String getFormacao() {
+    return formacao;
+  }
+
+  public void setFormacao(String formacao) {
+    this.formacao = formacao;
+  }
+
+  public String getNivelEscolaridade() {
+    return nivelEscolaridade;
+  }
+
+  public void setNivelEscolaridade(String nivelEscolaridade) {
+    this.nivelEscolaridade = nivelEscolaridade;
+  }
+
+  public String getObjetivos() {
+    return objetivos;
+  }
+
+  public void setObjetivos(String objetivos) {
+    this.objetivos = objetivos;
+  }
+
+  public String getHabilidades() {
+    return habilidades;
+  }
+
+  public void setHabilidades(String habilidades) {
+    this.habilidades = habilidades;
+  }
+
+  public String getExperiencias() {
+    return experiencias;
+  }
+
+  public void setExperiencias(String experiencias) {
+    this.experiencias = experiencias;
+  }
+}
+
+// Construtor da classe Curriculo
     // Inicializa os atributos com os valores fornecidos e define o status como falso
     public Curriculo(String nivelEscolaridade, String objetivos, String habilidades, 
                      String experiencia, String cursos) {
