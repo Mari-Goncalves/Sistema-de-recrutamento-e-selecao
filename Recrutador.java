bec72e41bf5e0e56296ae2228ee1c6aac8e3daf2
dbziv <147110388+dbziv@users.noreply.github.com>
2024-07-15 18:56:20 -0300

    Update Recrutador.java

--- Recrutador.java
+++ Recrutador.java
@@ -1,55 +1,76 @@
 import java.util.ArrayList;
-import java.util.List;
-
 public class Recrutador {
-    private String cargo; 
-    private String endereco;
-    private String nome; 
+  
+  private Empresa ugh;
+  String nome;
+  int id;
+  int senha;
+  String cargo;
+  String empresa;
 
-    // Lista de candidatos para armazenar os currículos
-    private List<Curriculo> candidatos;
 
-    // Construtor da classe Recrutador
-    public Recrutador(String cargo, String endereco, String nome) {
-        this.cargo = cargo;
-        this.endereco = endereco;
-        this.nome = nome;
-        this.candidatos = new ArrayList<>();
-    }
 
-    // Método para selecionar um candidato
-    public void selecionaCandidato(Curriculo candidato) {
-        candidato.setStatusCandidatouse(true);
-        candidatos.add(candidato);
-        System.out.println("Candidato selecionado com sucesso!");
-    }
+  protected Recrutador(String nome, int id, int senha, String cargo, String empresa){
+    this.nome = nome;
+    this.id = id;
+    this.senha = senha;
+    this.empresa = empresa;
 
-    // Método para excluir um candidato
-    public void excluiCandidato(Curriculo candidato) {
-        if (candidato.isStatusCandidatouse() && candidatos.remove(candidato)) {
-            System.out.println("Candidato excluído com sucesso!");
-        } else {
-            System.out.println("Usuário não pode ser removido ou não existe");
-        }
-    }
 
-    // Método para listar candidatos
-    public void listarCandidatosStatusContratado() {
-        System.out.println("Candidatos: ");
-        for (Curriculo candidato : candidatos) {
-            if (candidato.isStatusCandidatouse()) {
-                System.out.println("Nome: " + nome + ", Cargo: " + cargo + ", Endereço: " + endereco);
-            }
-        }
-    }
+  }
 
-    // Getters e setters 
-    public String getCargo() { return cargo; }
-    public void setCargo(String cargo) { this.cargo = cargo; }
+  public String getNome() {
+    return nome;
+  }
+  
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+  
+  public int getId() {
+    return id;
+  }
 
-    public String getEndereco() { return endereco; }
-    public void setEndereco(String endereco) { this.endereco = endereco; }
+  public void setId(int id) {
+    this.id = id;
+  }
 
-    public String getNome() { return nome; }
-    public void setNome(String nome) { this.nome = nome; }
+
+  public int getSenha() {
+    return senha;
+  }
+
+  public void setSenha(int senha) {
+    this.senha = senha;
+  }
+
+  public String getCargo() {
+    return cargo;
+  }
+
+  public void setCargo(String cargo) {
+    this.cargo = cargo;
+  }
+
+
+  public String getEmpresa() {
+    return empresa;
+  }
+
+  public void setEmpresa(String empresa) {
+    this.empresa = empresa;
+  }
+
+
+  @Override
+  public String toString() {
+
+    return nome + " " + cargo + " " + empresa + " " + id + " " + senha;
+  }
+
+
+
+  }
+
+  
 }
