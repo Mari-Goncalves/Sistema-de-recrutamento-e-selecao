4319aa63fec525ed12eadc1076491d24aea8d460
dbziv <147110388+dbziv@users.noreply.github.com>
2024-07-15 18:45:43 -0300

    Contem to string method

--- Empresa.java
+++ Empresa.java
@@ -1,8 +1,23 @@
 public class Empresa {
+    
+    
     private String razaoSocial;
     private String cnpj;
     private String endereco;
     private String emailCorporativo;
+    
+
+
+    protected Empresa(String razaoSocial, String cnpj, String endereco, String emailCorporativo){
+
+      this.razaoSocial = razaoSocial;
+      this.cnpj = cnpj;
+      this.endereco = endereco;
+      this.emailCorporativo = emailCorporativo;
+
+    }
+
+    
 
     public String getRazaoSocial() {
         return razaoSocial;
@@ -27,7 +42,7 @@ public class Empresa {
     public void setEndereco(String endereco) {
         this.endereco = endereco;
     }
-    
+
     public String getEmailCorporativo() {
         return emailCorporativo;
     }
@@ -36,4 +51,11 @@ public class Empresa {
         this.emailCorporativo = emailCorporativo;
     }
 
+
+     @Override
+        public String toString() {
+
+          return razaoSocial + " " + cnpj + " " + endereco + " " + emailCorporativo;
+        }
+    
 }
