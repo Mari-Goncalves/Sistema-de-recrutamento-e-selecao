c55670ddaf3d21632672dbea838602652aca7054
dbziv <147110388+dbziv@users.noreply.github.com>
2024-07-15 18:47:38 -0300

    Candidato contem o corpo do candidato

--- Candidato.java
+++ Candidato.java
@@ -1,113 +1,122 @@
-import java.util.Scanner;
+import java.util.ArrayList;
+public class Candidato{
 
-public class Candidato {
-    private String endereco;
-    private String cpf;
-    Curriculo curriculoCandidato = new Curriculo();
+  
+  
+    int id;
+    int senha;
+    String nome;
+    String email;
+    String nivelEscolaridade;
+    String objetivos;
+    String habilidades;
+    String experiencia;
+    String cursos;
+    
 
-    Scanner sc = new Scanner(System.in);
-    boolean isCurriculoCriado = false;
-    int opcaoMenuCandidato;
-    String atualizarExperiencia;
+    protected Candidato(int id, int senha, String nome, String email, String nivelEscolaridade, String objetivos, String habilidades, String experiencia, String cursos){
+      
+      this.id = id;
+      this.senha = senha;
+      this.nome = nome;
+      this.email = email;
+      this.nivelEscolaridade = nivelEscolaridade;
+      this.objetivos = objetivos;
+      this.habilidades = habilidades;
+      this.experiencia = experiencia;
+      this.cursos = cursos;
 
-    // Método construtor - 1º método chamado após a classe ser intanciada.
-    public Candidato() {
-        System.out.print("Olá candidato(a)! Você deseja: \n");
-        System.out.println(" ");
-        ExibirMenuCandidato();
-    }
-
-    public void ExibirOpcoesMenuCandidato() {
-        System.out.println("--------------------------------------------------");
-        System.out.println(" ");
-
-        System.out.print("1-Criar currículo\n");
-        System.out.print("2-Visualiar currículo\n");
-        System.out.print("3-Atualizar currículo\n");
-        System.out.print("4-Ver vagas disponiveis\n");
-        System.out.print("5-Listar candidaturas\n");
-
-        System.out.println(" ");
-    }
-
-    public void ValidarOpcaoMenuCandidato() {
-        System.out.print("Escolha uma opção: ");
-        opcaoMenuCandidato = sc.nextInt();
-
-        System.out.println(" ");
-        System.out.println("--------------------------------------------------");
-
-        switch (opcaoMenuCandidato) {
-            case 1:
-                if (!isCurriculoCriado) {
-                    CriarCurriculo();
-                } else {
-                    System.out.print("Seu currículo já foi criado. \n");
-                    ExibirMenuCandidato();
-                }
-                break;
-            case 2:
-                VisualizarCurriculo();
-                break;
-            case 3:
-                AtualizarCurriculo();
-                break;
-            case 4:
-                ListarVagasDisponiveis();
-                break;
-            case 5:
-                ListarCandidaturas();
-                break;
-            default:
-                System.out.print("Valor inválido! Tente novamente. \n");
-                ExibirMenuCandidato();
-                break;
-        }
-    }
-
-    public void ExibirMenuCandidato() {
-        ExibirOpcoesMenuCandidato();
-        ValidarOpcaoMenuCandidato();
-    }
-
-    public void CriarCurriculo() {
-        System.out.print("Opção 1 | Criar currículo\n");
-        curriculoCandidato.CriarCurriculo();
-        isCurriculoCriado = true;
-        ExibirMenuCandidato();
-    }
-
-    public void VisualizarCurriculo() {
-        if (isCurriculoCriado) {
-            System.out.print("Opção 2 | Visualizar currículo\n");
-            curriculoCandidato.VerResultadoCurriculo();
-        } else {
-            System.out.print("Seu currículo ainda não foi criado. \n");
-        }
-
-        ExibirMenuCandidato();
-    }
-
-    public void AtualizarCurriculo() {
-        if (isCurriculoCriado) {
-            System.out.print("Opção 3 | Atualizar currículo\n");
-            curriculoCandidato.AtualizarCurriculo();
-
-        } else {
-            System.out.print("Seu currículo ainda não foi criado. \n");
-        }
-
-        ExibirMenuCandidato();
 
     }
 
-    public void ListarVagasDisponiveis() {
-        System.out.print("ListarVagasDisponiveis... \n");
-        ExibirMenuCandidato();
+
+  
+
+
+    public String getNome() {
+      return nome;
     }
 
-    public void ListarCandidaturas() {
-        System.out.print("ListarCandidaturas... \n");
-        ExibirMenuCandidato();
+    public void setNome(String nome) {
+      this.nome = nome;
     }
-}
+
+    public int getId() {
+      return id;
+    }
+
+    public void setId(int id) {
+      this.id = id;
+    }
+
+
+    public int getSenha() {
+      return senha;
+    }
+
+    public void setSenha(int senha) {
+      this.senha = senha;
+    }
+
+    public void getEmail(String email){
+      this.email = email;
+    }
+
+    public String getEmail(){
+      return email;
+    }
+
+    public String getNivelEscolaridade() {
+      return nivelEscolaridade;;
+    }
+
+    public void setNivelEscolaridade(String nivelEscolaridade) {
+      this.nivelEscolaridade = nivelEscolaridade;
+    }
+
+
+    public String getObjetivos() {
+      return objetivos;
+    }
+
+    public void setObjetivos(String objetivos) {
+      this.objetivos = objetivos;
+    }
+
+    public String getHabilidades() {
+      return habilidades;
+    }
+
+   public void setHabilidades(String habilidades){
+     this.habilidades = habilidades;
+   }
+
+    public String getExperiencia() {
+      return experiencia;
+    }
+
+    public void setExperiencia(String experiencia) {
+      this.experiencia = experiencia;
+    }
+
+    public String getCursos() {
+      return cursos;
+    }
+
+    public void setCursos(String cursos) {
+      this.cursos = cursos;
+    }
+
+
+    @Override
+    public String toString() {
+
+      return id + " " + senha + " " + nome + " " + nivelEscolaridade + " " + objetivos + " " + id + " " + senha;
+    }
+
+
+
+    }
+
+
+  }
