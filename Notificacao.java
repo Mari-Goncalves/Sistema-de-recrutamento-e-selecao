af9cc500601cfddd1077b381e62b1ac58f5c9d8b
dbziv <147110388+dbziv@users.noreply.github.com>
2024-07-15 18:54:10 -0300

    Update Notificacao.java

--- Notificacao.java
+++ Notificacao.java
@@ -1,189 +1,141 @@
 
+
+import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Notificacao {
+    Scanner leitor = Main.getLeitor();
+    
     private Entrevista ent; //puxa entrevista para aviso de entrevista
     private DescricaoVaga nvg; 
+    private Vaga vg;
     private Candidato cnd; //puxa candidato
+    private Candidatura cand;
     private Empresa ugh;
     private StatusCandidatura att;
-    private Recrutador rec;
-    private Vaga vg; // puxa vagas
+    private Recrutador rh;
     private Menu mn;
+ 
 
-
-    // Method to add notification
-    
-       
+    // notificações candidato
+    protected void NotificacoesCnd() {
+        DescricaoVaga nova = vg.getNovaVaga();
+        int voltar;
         
-
-    //TROCAR METODO DE ADC VAGAS E METODO DE PROCURA DE VAGA DE LUGAR
-    // ADICIONAR RETURN PARA MENUS APROPRIADOS ABAIXO DAS NOTIFICACOES
-    // NECESSITA DE METODOS PARA DIFERENCIAR RECRUTADOR DO CANDIDATO
-    //
-       
-
-
-
-    // Method to display all notifications
-    protected void displayNotificacoes() {
-        
-        
-        
-            switch(att.getStatus){
+            switch(att.getStatus()){
                     
-                case att.ESTA_INSCRITO():       
+                case ESTA_INSCRITO:       
             
                 System.out.println("Você está a um passo de ser contratado!\n");
                 System.out.println("Mantenha-se atento ao agendamento da sua entrevista!");
                 System.out.println("1 - Voltar ");
-                    int voltar = leitor.nextInt();
+                    voltar = leitor.nextInt();
                     if(voltar == 1){
-                        mn.displayMainMenu();
+                        mn.MenuCandidato();
                     }
-               
-                case att.APTO_ENTREVISTA():
-                //if(recrutador){
-                System.out.print("");
-                //else{
-                System.out.println("");
+                    break;
                     
-                    System.out.println("1 - Voltar ");
-                    int voltar = leitor.nextInt();
-                    if(voltar == 1){
-                        mn.displayMainMenu();
-                    }
-                    
-                case att.CONTRATADO():
+                case CONTRATADO:
                 System.out.println("PARABÉNS, VOCÊ FOI CONTRADO/A!");
-                System.out.println("Fique atento as informações da vaga, entre em contato com " + rh.empresa + " pelo email " + rh.email);
-
+                System.out.println("Fique atento as informações da vaga, entre em contato com " + rh.getEmpresa() + " pelo email " + ugh.getEmailCorporativo());
                     System.out.println("1 - Voltar ");
-                    int voltar = leitor.nextInt();
+                    voltar = leitor.nextInt();
                     if(voltar == 1){
-                        mn.displayMainMenu();
+                        mn.MenuCandidato();
                     }
+                    break;
                     
-                case att.DESCLASSIFICADO():
-                System.out.println("Poxa, não foi dessa vez. Mas temos uma vaga para você! " + nvg.get(Vaga.size() -1));
+                case DESCLASSIFICADO:
+                System.out.println("Poxa, não foi dessa vez. Mas temos uma vaga para você! " + nova.getTitulo());
+                    System.out.println("1 - Voltar ");
+                    voltar = leitor.nextInt();
+                    if(voltar == 1){
+                        mn.MenuCandidato();
+                    }
+               break;
                     
+                case DESISTENCIA:
+                    System.out.println("Hmm... Talvez uma vaga em " + nova.getTitulo() + " seja melhor para você!");
                     System.out.println("1 - Voltar ");
-                    int voltar = leitor.nextInt();
+                    voltar = leitor.nextInt();
                     if(voltar == 1){
-                        mn.displayMainMenu();
+                        mn.MenuCandidato();
                     }
-                case att.PRIMEIRA_ENTREVISTA():
-                System.out.println("Boa sorte! Sua entrevista foi marcada para " + ent.getData() + "\n"");
-                System.out.println("Aqui vão algumas dicas parra garantir a vaga!"); 
+                    break;
 
-                    System.out.println("1 - Voltar ");
-                    int voltar = leitor.nextInt();
-                    if(voltar == 1){
-                        mn.displayMainMenu();
-                    }
-                    
-                case att.ENTREVISTA_TECNICA():
-                System.out.println("O que é uma entrevista técnica? Fique atento aos minimos detalhes para não passar vergonha na hora H");
+                default:
 
-                    System.out.println("1 - Voltar ");
-                    int voltar = leitor.nextInt();
-                    if(voltar == 1){
-                        mn.displayMainMenu();
-                    }
-
-                case att.ESTA_INSCRITO():
-
-                    System.out.println("1 - Voltar ");
-                    int voltar = leitor.nextInt();
-                    if(voltar == 1){
-                        mn.displayMainMenu();
-                    }
-
-                case att.DESISTENCIA():
-                    
-                  if(recrutador){
-                      System.out.println(cnd.getCandidato() + " infelizmente desistiu da vaga.");
-                      System.out.println("Cheque sua lista de candidatos para encontrar o perfeito candidato.");
-                      System.out.println("Já considerou " + cnd.get(Candidatos.size() -1) + " para a vaga? Temos uma excelente lista de candidatos para você!");
-                  }else{
-                    System.out.println("Hmm... Talvez uma vaga em " + nvg.get(Vaga.size() -1) + " seja melhor para você!");   
-                  }
-
-                    System.out.println("1 - Voltar ");
-                    int voltar = leitor.nextInt();
-                    if(voltar == 1){
-                        mn.displayMainMenu();
-                    }
-
-                    
-                case att.NAO_APTO_ENTREVISTA():
-
-                    if(recrutador){
-                        System.out.println("Para reagendar a entrevista, acesse nosso painel de agendamento.");
-                          }else{
-                        System.out.println("Entrevista não comparecida. Entre em contato com " + rh.empresa + " pelo email " + rh.email);
-                          }
-                    System.out.println("1 - Voltar ");
-                    int voltar = leitor.nextInt();
-                    if(voltar == 1){
-                        mn.displayMainMenu();
-                    }
-
-                case att.getStatus() == null:
-
-                    if(recrutador){
                         
-                        if(cnd.CandidatosList.isEmpty() -1) == null)){
-                            System.out.println("Não há candidatos para a vaga " + rh.getVaga() + ".");
-                            
-                        }else{
-                        System.out.println("Você tem " + Candidatos.size() + "\n");
-                        System.out.println("Wow! Parece que " + cnd.get(Candidatos.size()-1) + " se interessou na vaga!");
-                        
-                        for (Candidato cnd.getCandidato() : cnd.getCandidato()){
-                        System.out.println(cnd.get(Candidatos.size()-1) + " acaba de se inscrever para a vaga " + cnd.getVaga());
-                    }
-                          }else{
-                        
-                        System.out.println(ugh.getEmpresa() + " esta contratando para " + vg.getTitulo() + "\n");
+                        System.out.println(nova.getEmpresa() + " esta contratando para " + nova.getTitulo() + "\n");
                         System.out.println("Você está a um passo de ser contratado!\n");
-                        System.out.println("Boa Sorte! Sua entrevista foi marcada para " + ent.getData() + "\n");
-                        
-                          }
-                    System.out.println("1 - Voltar ");
-                    int voltar = leitor.nextInt();
-                    if(voltar == 1){
-                        mn.displayMainMenu();
-                    }
-                    
-                    
-
+                        System.out.println("Boa Sorte! Sua entrevista foi agendada. Cheque o menu de agendamento para mais informações.\n");
+                        System.out.println("1 - Voltar ");
+                        voltar = leitor.nextInt();
+                        if(voltar == 1){
+                        mn.MenuCandidato();
+                    break;
+                        }
                 
             }
         }
+
+
+           
+            protected void NotificacoesRh() {
+                Candidatura nova = cand.getPretendente();
+                int voltar;
+
+            switch(att.getStatus()){
+                    
+                case ESTA_INSCRITO:
+
+                    System.out.println(nova.getNome() + " acaba de se candidatar para " + nova.getVaga());
+                    System.out.println("1 - Voltar ");
+                    voltar = leitor.nextInt();
+                    if(voltar == 1){
+                        mn.MenuRecrutador();
+                    }
+                    break;
+                    
+                case DESISTENCIA:
+
+                        //se o candidato desiste.
+                          System.out.println(cnd.getNome() + " infelizmente desistiu da vaga.");
+                          System.out.println("Cheque sua lista de candidatos para encontrar o perfeito candidato.");
+                          System.out.println("Já considerou " + nova.getNome() + " para a vaga? Temos uma excelente lista de candidatos para você!");
+
+                    System.out.println("1 - Voltar\n2 - Candidaturas\n__");
+                    voltar = leitor.nextInt();
+                    if(voltar == 1){
+                        mn.MenuRecrutador();
+            }else{
+                        cand.Candidaturas();
+            }
+                    break;
+                    
+                default:
+                    
+                        if(cand.getCandidatura().isEmpty()) {
+                            System.out.println("Não há candidatos para a vaga " + nvg.getTitulo() + ".");
+                            System.out.println("1 - Voltar\n__");
+
+                        }else{
+
+                            System.out.println(nova.getNome() + " acaba de se inscrever para a vaga ");
+                            System.out.println("1 - Voltar\n__");
+                            
+                            }
+                            
+                        }
+            }
+                    
+    
     }
 
 
- 
-
-    private void Painel(Entrevista ent){
-        this.ent = ent;
-    }
-    private void Painel(DescricaoVaga nvg){
-        this.nvg = nvg;
-    }
-    private void Painel(Candidato cnd){
-        this.cnd = cnd;
-    }
-    private void Painel(Empresa ugh){
-        this.ugh = ugh;
-    }
-    private void Painel(StatusCandidatura att){
-        this.att = att;
-    }
     
 
     
 }
 
- //dished damned and done, bitch
+
