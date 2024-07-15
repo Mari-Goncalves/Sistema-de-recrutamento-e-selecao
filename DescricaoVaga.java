2bc6995bb352dedc8d3244224d47fa147ba16811
dbziv <147110388+dbziv@users.noreply.github.com>
2024-07-15 18:49:45 -0300

    Nem sei o que foi atualizado, perdi a linha da documentaçao

--- DescricaoVaga.java
+++ DescricaoVaga.java
@@ -1,4 +1,4 @@
-public class DescricaoVaga{
+public class DescricaoVaga extends Vaga{
     public int id;
     public String titulo;
     public String descricao;
@@ -7,10 +7,12 @@ public class DescricaoVaga{
     public String cargaHoraria;
     public String tipoContratacao;
     public String modalidade;
+    public String empresa;
+    private Empresa ugh;
     
     
     
-    public DescricaoVaga(int id, String titulo, String descricao, String requisito, String salario, String cargaHoraria, String tipoContratacao, String modalidade){
+    public DescricaoVaga(int id, String titulo, String descricao, String requisito, String salario, String cargaHoraria, String tipoContratacao, String modalidade, String empresa){
         this.id = id;
         this.titulo = titulo;
         this.descricao = descricao;
@@ -19,6 +21,7 @@ public class DescricaoVaga{
         this.cargaHoraria = cargaHoraria;
         this.tipoContratacao = tipoContratacao;
         this.modalidade = modalidade;
+        this.empresa = ugh.getRazaoSocial();
 
     }
     
@@ -87,11 +90,19 @@ public class DescricaoVaga{
         this.modalidade = modalidade;
     }
 
+    public String getEmpresa() {
+        return empresa;
+    }
+
+    public void setEmpresa(String empresa) {
+        this.empresa = empresa;
+    }
+
 
     @Override
 public String toString() {
        
-        return titulo + " " + salario + " " + modalidade + " " + cargaHoraria + tipoContratacao + modalidade; 
+        return titulo + " " + salario + " " + modalidade + " " + cargaHoraria + tipoContratacao + modalidade + " " + empresa; 
 }
     
  
